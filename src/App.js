cb22860ab688cb212cee8acc56d99702b2cd79c0
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-06 10:05:55 -0400

    ADDED initial layout and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Err404 from "./pages/Err404";
+import Quiz from "./pages/Quiz"
+
 import './styles/App.css';
 
 function App() {
   return (
     <>
-      test
+      <BrowserRouter>
+        <Routes>
+          <Route path='/quiz'  element={ <Quiz />} />
+          <Route path="/" element={ <Home /> } />
+          <Route path='*' element={ <Err404 /> } />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
